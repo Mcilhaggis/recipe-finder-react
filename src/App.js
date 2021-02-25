7f606b2c3fcea80dc33222924b5cfba36bd5b307
Rachael McIlhagga <rachael.mcilhagga@live.co.uk>
2021-02-25 13:13:13 -0500

    env file changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import Recipe from './Recipe';
 import './App.css';
+import dotenv from  'dotenv'
 
 
 
+  const APP_ID = process.env.REACT_APP_APP_ID;
+  const APP_KEY = process.env.REACT_APP_APP_KEY;
+
+
+  console.log(process.env.REACT_APP_APP_ID)
 function App() {
-
-  let REACT_APP_API_ID;
-  let REACT_APP_API_KEY;
-
 
   const [recipes, setRecipes] = useState([]);
 
@@ -24,7 +26,7 @@ function App() {
 
   //make async call for recipes 
   const getRecipes = async () => {
-    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${REACT_APP_API_ID}&app_key=${REACT_APP_API_KEY}`)
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`)
     const data = await response.json();
     console.log(data)
     setRecipes(data.hits)
